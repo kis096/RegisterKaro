d93a838bb3b61941299f68959ccf9b1d586167bd
Kishan <ranakishan3050@gmail.com>
2025-01-22 19:37:00 +0530

    Updated index.html, App.js, and CSS for responsiveness

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';  // Import your CSS file here
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components'; 
+import { ThemeProvider } from 'styled-components';
 
 import Home from './Components/Home';
 import OurService from './Components/OurService';
